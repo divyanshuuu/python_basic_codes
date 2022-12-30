4ca857b0fbbadbb5c6344b467c7fe4bf1506b4a4
divyanshuuu <54057001+divyanshuuu@users.noreply.github.com>
2022-12-30 19:44:27 +0530

    Add files via upload

---
+++ advanced_string_functions.py
@@ -0,0 +1,24 @@
+name="divyanshu"
+print(name[0])#first letter
+print(name[1:3])
+print(name[-1])#last letter
+sentence="Hello Brother"
+print(sentence.split())
+s_p=sentence.split()
+s_j=' '.join(s_p)
+print(s_j)
+quote="He said, 'To be old and wise you must first be young and stupid'."
+print(quote)
+quote="He said, \"To be old and wise you must first be young and stupid\"."
+print(quote)
+space="                      hellino        "
+print(space)
+print(space.strip())
+letter="A"
+word="Apple"
+print("A" in "Apple")
+print(letter.lower())
+manwha="Second Life Ranker"
+print("My favourite manwha is {}.".format(manwha))
+print("My favourite manwha is %s." % manwha)
+print(f"My favourite manwha is {manwha}.")
